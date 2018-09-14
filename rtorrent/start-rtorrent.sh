c953a38bc3a81790f5ed2d2abea93c7676e83337
William Kray <william@kray.pw>
2018-09-14 01:51:06 -0700

    fix lockfile issues with unclean shutdown

--- rtorrent/start-rtorrent.sh
+++ rtorrent/start-rtorrent.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+if [ -f /home/rtorrent/.rt_session/rtorrent.lock ]; then
+  rm /home/rtorrent/.rt_session/rtorrent.lock
+fi
+
 screen -dmS torrent rtorrent
 touch /tmp/rtorrent.log
 exec tail -f /tmp/rtorrent.log
